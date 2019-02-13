443b84883066662fef212654fafc35fcb3181b0f
lucianaribeiro <albuquerqueluciana54@gmail.com>
2019-02-13 15:17:20 -0200

    Finishin second frame

--- src/components/frame2/bio.js
+++ src/components/frame2/bio.js
@@ -5,24 +5,22 @@ class Bio extends Component{
 
     render(){
         return(
-            <div className="">
-                <div className="row justify-content-around align-items-center">
-                        <div className="col-8">
-                            <div>
-                                <div className="title-frame2">
-                                    <img src={logo} alt="logo" className="logo-title"/>
-                                </div>
-                            </div>
-                            <div className="sub-content text-white">
-                                <h6 class="text-left">
-                                    O <span>Quatro Patas</span> oferece para você diversas formas práticas e simples 
-                                    de <span>cuidar</span> do seu <span>bichinho</span> com apenas alguns cliques utilizando o
-                                     <span> aplicativo</span>.
-                                </h6>
+            <div className="row justify-content-around align-items-center">
+                    <div className="col-12">
+                        <div>
+                            <div className="title-frame2">
+                                <img src={logo} alt="logo" className="logo-title"/>
                             </div>
                         </div>
+                        <div className="sub-content text-white">
+                            <h5 class="text-left">
+                                O <span>Quatro Patas</span> oferece para você diversas formas práticas e simples 
+                                de <span>cuidar</span> do seu <span>bichinho</span> com apenas alguns cliques utilizando o
+                                    <span> aplicativo</span>.
+                            </h5>
+                        </div>
                     </div>
-            </div>
+                </div>
         );
     }
 }
--- src/components/frame2/frame2.js
+++ src/components/frame2/frame2.js
@@ -7,13 +7,15 @@ class Frame2 extends Component{
  
     render(){
         return(
-            <div className="frame2-img">
-                <div className="content-container">
+            <div className="container"> 
+            <div className="frame2-img row justify-content-around">
+                <div className="col-4">
+                    <img src={cell} alt="cellphone" className="conjunto-img"/>
+                </div>
+                <div className="col-4">
                     <Bio />
                 </div>
-                <div>
-                    <img src={cell} alt="cellphone" className="cell-img"/>
-                </div>
+            </div>
             </div>
         );
     }
